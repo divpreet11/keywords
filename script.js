c5183eaed9ea189eed6e301ac314d394f306042e
divpreet11 <divpreetkaur08@gmail.com>
2024-04-09 23:22:14 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Void keyword
+
+//Example-1
+var x,y,z;    
+x = void ( y = 50, z = 70 );  
+console.log('x = ' + x + ' y = ' + y +' z = ' + z );  
+
+//Example-2
+void function () {
+    console.log("Executed!");
+  }();
+
+//Example-3
+void function hello() {   
+    var msg = function ()   
+    {console.log("Welcome back!!")};   
+    msg();   
+ }(); 
